a3ae30fc2cd0ab1e5cc56240b6d8c8609bb9245e
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-09-27 13:14:58 +0300

    override ToString

---
+++ Task5_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectanle = new Rectangle(5, 10);
+            Figure figure = new Figure(120);
+            figure.GetQuantityOfRectanglesInFigure(rectanle.GetAreaOfRectangle());
+            Console.WriteLine(figure.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task5_4/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_4
+{
+    class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public double GetAreaOfRectangle()
+        {
+           return Width* Height;
+        }
+    }
+    class Figure
+    {
+        public double AreaOfFigure { get; set; }
+        public double QuantityOfRectangles { get; set; }
+        public Figure(double areaOfFigure)
+        {
+            AreaOfFigure = areaOfFigure;
+        }
+        public double GetQuantityOfRectanglesInFigure(double areaOfRectangle)
+        {
+            return QuantityOfRectangles=AreaOfFigure/areaOfRectangle;
+        }
+        public override string ToString()
+        {
+            string result = string.Format("{0:0.##}", QuantityOfRectangles);
+            return $"Result is: {result}";
+        }
+    }
+}
